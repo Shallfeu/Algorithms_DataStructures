2cb7dc648bb500210b49898fcd7f970b3f62b4ff
Shallfeu <chigvincevra@mail.ru>
2023-01-18 14:07:10 +0300

    deep merge - done

---
+++ deep_merge_and_compare/src/deepCompare.js
@@ -0,0 +1,18 @@
+const { isObject, isArray, isSameType } = require('./helpers');
+
+module.exports = function deepCompare(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if ((!isObject(obj1) && !isArray(obj1)) || (!isObject(obj2) && !isArray(obj2))) return false;
+
+  if (!isSameType(obj1, obj2) || Object.keys(obj1).length !== Object.keys(obj2).length)
+    return false;
+
+  for (const key of Object.keys(obj1)) {
+    if (!obj2.hasOwnProperty(key)) return false;
+
+    if (!deepCompare(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+};
---
+++ deep_merge_and_compare/src/deepMerge.js
@@ -0,0 +1,86 @@
+const { isObject, isArray, isSameType } = require('./helpers');
+
+module.exports = deepMerge;
+
+function deepMerge(obj1, obj2) {
+  if ((!isObject(obj1) && !isArray(obj1)) || !isSameType(obj1, obj2)) {
+    if (isObject(obj2)) {
+      return deepCopyObject(obj2);
+    }
+
+    if (isArray(obj2)) {
+      return deepCopyArray(obj2);
+    }
+
+    return obj2;
+  }
+
+  if (isArray(obj1)) return deepMergeArrays(obj1, obj2);
+
+  return deepMergeObjects(obj1, obj2);
+}
+
+function deepCopyObject(obj) {
+  const result = { ...obj };
+
+  for (const i of Object.keys(obj)) {
+    if (isArray(result[i])) {
+      result[i] = deepCopyArray(result[i]);
+      continue;
+    }
+
+    if (isObject(result[i])) {
+      result[i] = deepCopyObject(result[i]);
+      continue;
+    }
+  }
+
+  return result;
+}
+
+function deepCopyArray(arr) {
+  const result = [...arr];
+
+  for (let i = 0; i < result.length; i += 1) {
+    if (isArray(result[i])) {
+      result[i] = deepCopyArray(result[i]);
+      continue;
+    }
+
+    if (isObject(result[i])) {
+      result[i] = deepCopyObject(result[i]);
+      continue;
+    }
+  }
+
+  return result;
+}
+
+function deepMergeArrays(arr1, arr2) {
+  return deepCopyArray([...arr1, ...arr2]);
+}
+
+function deepMergeObjects(obj1, obj2) {
+  const result = deepCopyObject(obj1);
+
+  for (const key of Object.keys(obj2)) {
+    if (!result.hasOwnProperty(key)) {
+      if (isObject(obj2[key])) {
+        result[key] = deepCopyObject(obj2[key]);
+        continue;
+      }
+
+      if (isArray(obj2[key])) {
+        result[key] = deepCopyArray(obj2[key]);
+        continue;
+      }
+
+      result[key] = obj2[key];
+      continue;
+    }
+
+    result[key] = deepMerge(result[key], obj2[key]);
+  }
+
+  return result;
+}
